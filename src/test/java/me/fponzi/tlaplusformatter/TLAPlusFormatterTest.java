fe67b892b24bb71547adb9110054166afb1c64b2
Federico Ponzi <me@fponzi.me>
2024-07-28 12:13:47 +0100

    Compare AST of the input and output specs

--- src/test/java/me/fponzi/tlaplusformatter/TLAPlusFormatterTest.java
+++ src/test/java/me/fponzi/tlaplusformatter/TLAPlusFormatterTest.java
@@ -2,6 +2,7 @@ package me.fponzi.tlaplusformatter;
 
 import org.junit.jupiter.api.Test;
 import tla2sany.drivers.FrontEndException;
+import tla2sany.st.TreeNode;
 
 import java.io.File;
 import java.io.IOException;
@@ -12,6 +13,25 @@ import java.nio.file.Path;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TLAPlusFormatterTest {
+
+    boolean compareAst(TreeNode root1, TreeNode root2) {
+        if (root1.zero() != null) {
+            for (int i = 0; i < root1.zero().length ; i++) {
+                if(!compareAst(root1.zero()[i], root2.zero()[i])) {
+                    return false;
+                }
+            }
+        }
+        if (root1.one() != null) {
+            for (int i = 0; i < root1.one().length ; i++) {
+                if(!compareAst(root1.one()[i], root2.one()[i])) {
+                    return false;
+                }
+            }
+        }
+        return root1.getImage().equals(root2.getImage());
+    }
+
     // TODO: compare AST of pre format and post format.
     /**
      * Compares src/test/resources/inputs/name.tla to src/test/resources/outputs/name.tla
@@ -30,15 +50,24 @@ class TLAPlusFormatterTest {
             assertNotNull(actual, "Formatted output is null");
             assertNotNull(expected, "Expected output is null");
             assertEquals(expected, actual, "Formatted output does not match expected output");
+
             // here we use the downside of having SANY validating the spec
             // as an advantage to ensure the specs are valid.
             // also, the formatting should be stable.
+
+            // initialize tlaplusfmt using output file path.
+            // in this way, if the spec EXTENDS other specs, we can include them in the outputs resource folder.
+            // For example see TowerOfHanoi.tla.
+            // If output is an invalid spec, SANY will let us know.
+            var f2 = new TLAPlusFormatter(new File(outputFile.toURI()));
+
+            // the ast of the initial spec should match the ast of the output spec.
+            assertTrue(compareAst(f.root, f2.root));
+
             try {
-                // initialize tlaplusfmt using output file path.
-                // in this way, if the spec EXTENDS other specs, we can include them in the outputs resource folder.
-                // For example see TowerOfHanoi.tla.
-                f = new TLAPlusFormatter(new File(outputFile.toURI()));
-                actual = f.getOutput();
+                // It should be a bit redundant with the compareAst above, but it's just an additional sanity check.
+                // might remove later to keep tests fast
+                actual = f2.getOutput();
                 assertNotNull(actual, "Formatted output is null");
                 assertEquals(expected, actual, "Second formatted output does not match expected output");
             } catch (Exception e) {
