ac4ab6f6ebe88e3b313a0c37f773c020802b26f1
Federico Ponzi <me@fponzi.me>
2025-08-30 20:53:19 +0100

    Add submodules tests

---
+++ src/test/java/me/fponzi/tlaplusformatter/SubmodulesTest.java
@@ -0,0 +1,11 @@
+package me.fponzi.tlaplusformatter;
+
+import org.junit.jupiter.api.Test;
+
+public class SubmodulesTest extends LexiconTest {
+
+    @Test
+    void testSubmodules() {
+        testSpecFiles("Submodules");
+    }
+}
