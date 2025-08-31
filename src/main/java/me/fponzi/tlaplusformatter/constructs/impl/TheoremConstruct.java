c7076e21da3261c2e69118c77e0801b63c590a4b
Federico Ponzi <me@fponzi.me>
2025-08-31 22:23:11 +0100

    Fix theorem construct

--- src/main/java/me/fponzi/tlaplusformatter/constructs/impl/TheoremConstruct.java
+++ src/main/java/me/fponzi/tlaplusformatter/constructs/impl/TheoremConstruct.java
@@ -3,6 +3,7 @@ package me.fponzi.tlaplusformatter.constructs.impl;
 
 import com.opencastsoftware.prettier4j.Doc;
 import me.fponzi.tlaplusformatter.constructs.ConstructContext;
+import me.fponzi.tlaplusformatter.constructs.NodeKind;
 import me.fponzi.tlaplusformatter.constructs.TlaConstruct;
 import tla2sany.st.TreeNode;
 
@@ -18,21 +19,17 @@ public class TheoremConstruct implements TlaConstruct {
 
     @Override
     public int getSupportedNodeKind() {
-        return -1;
-        // TODO:
-        //return NodeKind.THEOREM.getId();
+        return NodeKind.THEOREM.getId();
     }
 
     @Override
     public Doc buildDoc(TreeNode node, ConstructContext context, int indentSize) {
-        if (node.one() != null && node.one().length > 0) {
-            Doc expression = context.buildChild(node.one()[0]);
-            return Doc.group(
-                    Doc.text("THEOREM")
-                            .appendLine(expression.indent(context.getConfig().getIndentSize()))
-            );
-        }
-
-        return Doc.text("THEOREM");
+        var z = node.zero();
+        assert (z != null && z.length > 0);
+        var theoremName = context.buildChild(z[0]);
+        var expr = context.buildChild(z[1]);
+        return Doc.group(
+                theoremName.appendLineOrSpace(expr)
+        ).indent(z[0].getImage().length() + 1);
     }
 }
