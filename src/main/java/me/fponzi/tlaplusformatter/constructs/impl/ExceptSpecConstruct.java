f65001cc219d29da243c3b4ca521240d508dfb3b
Federico Ponzi <me@fponzi.me>
2026-02-02 18:07:23 +0000

    Fix expect formatting

--- src/main/java/me/fponzi/tlaplusformatter/constructs/impl/ExceptSpecConstruct.java
+++ src/main/java/me/fponzi/tlaplusformatter/constructs/impl/ExceptSpecConstruct.java
@@ -7,7 +7,8 @@ import me.fponzi.tlaplusformatter.constructs.TlaConstruct;
 import tla2sany.st.TreeNode;
 
 /**
- * Example: ![from]=towers[from]-disk
+ * Handles EXCEPT specifications like ![from]=towers[from]-disk
+ * Also handles chained components like ![r].smoking = FALSE
  */
 public class ExceptSpecConstruct implements TlaConstruct {
     @Override
@@ -24,12 +25,26 @@ public class ExceptSpecConstruct implements TlaConstruct {
     public Doc buildDoc(TreeNode node, ConstructContext context, int indentSize) {
         var z = node.zero();
         assert (z != null);
+        // Structure: ! <ExceptComponent>+ = <value>
+        // There can be multiple ExceptComponents (e.g., ![r].smoking = FALSE)
+        // - z[0] is always the bang (!)
+        // - z[1..n-2] are ExceptComponents
+        // - z[n-1] is the equals sign (=)
+        // - z[n] is the value expression
+        assert (z.length >= 4); // minimum: ! comp = value
+
         var bang = context.buildChild(z[0]);
-        var exceptComp = context.buildChild(z[1]);
-        var eq = context.buildChild(z[2]);
-        var infixExpr = context.buildChild(z[3]);
+
+        // Build all except components (indices 1 to length-3)
+        Doc exceptComps = context.buildChild(z[1]);
+        for (int i = 2; i < z.length - 2; i++) {
+            exceptComps = exceptComps.append(context.buildChild(z[i]));
+        }
+
+        var eq = context.buildChild(z[z.length - 2]);
+        var infixExpr = context.buildChild(z[z.length - 1]);
         return bang
-                .append(exceptComp)
+                .append(exceptComps)
                 .appendSpace(eq)
                 .appendLineOrSpace(infixExpr);
     }
